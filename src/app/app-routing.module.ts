8a93f625d4d0308bcbaad0811d2c974c8a9674f4
atchuthkumar96 <atchuthkumar96@gmail.com>
2022-03-02 12:07:37 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,40 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { UsersComponent } from './users/users.component';
+import { AlbumsComponent } from './albums/albums.component';
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+const routes: Routes = [
+  {
+    path: ' ',
+    component: UsersComponent,
+    pathMatch:'full'
+    
+  },
+  
+  {
+    path: 'users',
+    component: UsersComponent,
+    
+  },
+  
+  
+
+  {
+    path: 'albums',
+    component: AlbumsComponent,
+  },
+  {
+    path: '',
+    component: AppComponent,
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatButtonModule} from '@angular/material/button';
 
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './user/user.component';
+import { AlbumsComponent } from './albums/albums.component';
+// import { FlexLayoutModule } from '@angular/flex-layout';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    AlbumsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatButtonModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatCardModule,
+    // FlexLayoutModule,
+    MatGridListModule,
+    MatIconModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
